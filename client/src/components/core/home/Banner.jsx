e2444bc1b67f30f08c7f6f9f3436f44f34159235
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2024-09-25 15:49:45 +0530

    ..

--- client/src/components/core/home/Banner.jsx
+++ client/src/components/core/home/Banner.jsx
@@ -1,7 +1,31 @@
 import React from "react";
-
+import image from "../../../assests/bannersec.jpg";
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="bg-[#d8d6e4] min-h-[300px] w-full -mb-[10px] ">
+      <div className=" flex w-[80%] mx-auto items-center justify-between text-[#323232]">
+        <div>
+          <div className=" flex flex-col justify-start gap-6">
+            <p className=" text-3xl font-bold lg:w-[60%] ">Experience our award winning service & support!</p>
+            <p className=" text-xl">
+              One of the Most Awarded Financial Service Provider in the Region
+            </p>
+
+            <div className=" flex   ">
+            <button className="p-4 bg-[#f5c300] rounded-full font-bold">
+            Learn More
+
+            </button>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <img src={image} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- client/src/components/core/home/LastBanner.jsx
+++ client/src/components/core/home/LastBanner.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React from 'react';
 
-const LastBanner = () => {
-  return <div>LastBanner</div>;
+const PartnershipBlock = () => {
+  return (
+    <div className="partnership-blk py-4 relative z-0">
+      <div className="container mx-auto py-5">
+        <div className="flex justify-end py-5">
+          <div className=" lg:w-[430px] lg:absolute top-[100px] right-[150px] w-full text-center pt-4 lg:text-left ml-3 text-[#323232] ">
+            {/* Large Screen Heading */}
+            <h2 className="text-5xl font-bold  mb-3 hidden lg:flex wow fadeInUp ">
+              You are independent.<br />
+              But never alone.
+            </h2>
+
+            {/* Small Screen Heading */}
+            <h2 className="text-4xl font-bold  mb-3 lg:hidden wow fadeInUp ">
+              You are independent.<br />
+              But never alone.
+            </h2>
+
+            <p className="text-lg font-normal pb-5 mb-5 lg:mb-0 wow fadeInUp mt-6" data-wow-delay="0.5s">
+              Good investments are all about the right decisions. And we're here to support you through our in-depth market briefings, around-the-clock alerts, investment options, training, and more.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default LastBanner;
+export default PartnershipBlock;
