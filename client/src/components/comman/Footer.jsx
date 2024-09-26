933a3a583350084f39dd2b8d3bd3ed510cc6785e
Rishi <rishimaheshwari040@gmail.com>
2024-09-26 15:31:10 +0530

    up

--- client/src/components/comman/Footer.jsx
+++ client/src/components/comman/Footer.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram, FaYoutube } from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaInstagram,
+  FaYoutube,
+} from "react-icons/fa";
 
-import logo from '../../assets/logo.png'
-import img1 from '../../assets/footer/1.png'
-import img2 from '../../assets/footer/2.png'
-import img3 from '../../assets/footer/3.png'
-import img4 from '../../assets/footer/4.png'
-import img5 from '../../assets/footer/5.png'
-import img6 from '../../assets/footer/6.png'
-
-
+import logo from "../../assets/logo.png";
+import img1 from "../../assets/footer/1.png";
+import img2 from "../../assets/footer/2.png";
+import img3 from "../../assets/footer/3.png";
+import img4 from "../../assets/footer/4.png";
+import img5 from "../../assets/footer/5.png";
+import img6 from "../../assets/footer/6.png";
 
 const navigation = [
   {
@@ -63,59 +67,70 @@ const navigation = [
   },
 ];
 
-
 const Footer = () => {
-  return <div className=" w-[90%] mx-auto text-[#323232]">
+  return (
+    <div className=" w-[90%] mx-auto text-[#323232]">
+      {/* First section */}
 
+      <div className=" flex w-full mb-16">
+        {/* logo section */}
+        <div className=" w-[25%] flex flex-col gap-3">
+          <div>
+            <img src={logo} alt="TradegyanLogo" />
+          </div>
 
-{/* First section */}
+          <div>
+            <h3 className=" font-bold text-xl">Regulated by SCA</h3>
+            <p className=" text-sm">
+              Century Financial is a registered trademark of Century Financial
+              Consultancy LLC
+            </p>
+          </div>
 
-<div className=" flex w-full mb-16">
+          <div className="flex space-x-4 ">
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookF className="text-2xl text-[#3b5998] hover:text-gray-600" />
+            </a>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter className="text-2xl text-[#00acee] hover:text-gray-600" />
+            </a>
+            <a
+              href="https://www.linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedinIn className="text-2xl text-[#0077b5] hover:text-gray-600" />
+            </a>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram className="text-2xl text-[#C13584] hover:text-gray-600" />
+            </a>
+            <a
+              href="https://www.youtube.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaYoutube className="text-2xl text-[#FF0000] hover:text-gray-600" />
+            </a>
+          </div>
+        </div>
 
-{/* logo section */}
-  <div className=" w-[25%] flex flex-col gap-3">
-
-<div>
-  <img src={logo} alt="TradegyanLogo" />
- 
-</div>
-
-<div>
-<h3 className=" font-bold text-xl">Regulated by SCA</h3>
-<p className=" text-sm">Century Financial is a registered trademark of
-Century Financial Consultancy LLC</p>
-
-</div>
-
-<div className="flex space-x-4 ">
-      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-        <FaFacebookF className="text-2xl text-[#3b5998] hover:text-gray-600" />
-      </a>
-      <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-        <FaTwitter className="text-2xl text-[#00acee] hover:text-gray-600" />
-      </a>
-      <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-        <FaLinkedinIn className="text-2xl text-[#0077b5] hover:text-gray-600" />
-      </a>
-      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-        <FaInstagram className="text-2xl text-[#C13584] hover:text-gray-600" />
-      </a>
-      <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
-        <FaYoutube className="text-2xl text-[#FF0000] hover:text-gray-600" />
-      </a>
+        {/* Links */}
+        <div></div>
+      </div>
     </div>
-  </div>
-
-
-{/* Links */}
-  <div>
-
-  </div>
-</div>
-
-
-
-  </div>;
+  );
 };
 
 export default Footer;
