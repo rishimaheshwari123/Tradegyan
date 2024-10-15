d4760681fc5076c26d4e063aa47963465d83410e
Rishi <rishimaheshwari040@gmail.com>
2024-10-15 15:08:56 +0530

    update

--- client/src/pages/Career.jsx
+++ client/src/pages/Career.jsx
@@ -5,94 +5,24 @@ import Navbar from "../components/comman/Navbar";
 
 export const jobPostings = [
   {
-    title: "What kind of company is Trade Gyan Solutions",
-    details:
-      "Trade Gyan Solutions is an Investment Advisor- purely research company focusing on Technical Analysis that provides real-time financial markets recommendation (Trading Advice) to its customers for intra-day and short term trading on real-time data.",
-  },
-  {
-    title: "What is the authenticity of your company?",
-    details:
-      "We follow strict policies in our organization, and for more you can visit our website www.tradegyan.co. And you are always welcomed to visit our office too.",
-  },
-  {
-    title: "What Services do you provide?",
-    details:
-      "We provide the recommendations of Equity, Derivative and Commodity Market.",
-  },
-  {
-    title:
-      "What is the Minimum Investment requirement for Trading in order to get your calls?",
-    details:
-      "There is no such minimum investment required for our calls. First of all, you need to check your risk appetite. You can choose the lot size & shares according to your convenience but it would be easier for you to have at least 50k to trade in intraday efficiently.",
-  },
-  {
-    title: "Is there any Free-Trial Service? If yes, how can I avail that?",
-    details:
-      "No, We are not providing any free trial service according to SEBI new Rules (https://bit.ly/2MHXnlG), See our Traksheet and Ask for service",
-  },
-  {
-    title: "What will be the mode of Recommendations you provide?",
-    details:
-      "You can avail the recommendations at all Digital Platforms like (Only Phone SMS).",
-  },
-  {
-    title: "What is your Success / Accuracy / Hit Ratio?",
-    details: "We provide good accuracy on a consistent basis.",
-  },
-  {
-    title: "How to contact you in the Market hours?",
-    details:
-      "You can anytime call us at our Support Number: +91-7771004878, you can contact to our specific departments.",
-  },
-  {
-    title: "What are your paid services?",
-    details:
-      "You can find the various Services / Products offered by us in the Pricing section of our Website: Trade Gyan Pricing.",
-  },
-  {
-    title: "What is the procedure to subscribe to your services?",
-    details:
-      "You can fill up the Subscription Form available on the Website or simply talk to our Support Team 24/7 for assistance at +91-7771004878.",
-  },
-  {
-    title: "What is the Best Trading Style you recommend for the trader?",
-    details:
-      "We always suggest Intraday Trading to our customers because, in the present scenario, the Indian markets are mostly governed by the global trends which can change in a day or night. Hence, most of our calls are purely intraday.",
-  },
-  {
-    title: "Do you provide some special services?",
-    details:
-      "Yes, we have a Premium & Special pack for the investors and traders who trade in heavy lots and their risk appetite should be high.",
-  },
-  {
-    title: "Do you provide the detailed analysis of the market?",
-    details:
-      "Yes, we provide the Weekly Premium Reports, Daily Premium Newsletter, and Daily Technical Levels, which help you to trade and invest better in the market.",
-  },
-  {
-    title: "Can we directly talk to the analysts?",
-    details:
-      "Yes, you can directly contact to our representatives, as when you get subscribed with us one of our representatives takes your regular follow-up and you can contact your representative any time at his/her extensions or you can call us 24/7 at our hotline number +91-7771004878 and talk directly to our Analysts and get complete support on your Positions.",
-  },
-  {
-    title: "Do you recommend taking delivery?",
-    details:
-      "Predominantly we have intraday packages and generally, we always suggest our customers close their Positions on the same day but at the same time, we have some packages for delivery as well.",
-  },
-  {
-    title: "Do you suggest when can I book a profit or exit?",
-    details:
-      "Yes, we provide complete follow-ups for both Stop-Loss & Targets via SMS only until you close your position, as we use strong technologies to provide the best services to our clients.",
-  },
-  {
-    title: "Is there any support helpline available?",
-    details:
-      "We provide the quality services to our clients but still if you have any queries and support, then you can directly contact to our support and Grievances Department 24/7 by contacting at +91-7771004878 or you can write us at 121@tradegyan.co .",
-  },
-  {
-    title: "Is there any live support facility available?",
-    details:
-      "Yes, we provide live support to our customers as we believe in giving the best solutions at the right time for your queries. For live support, you need to visit our website or can follow the link: Trade Gyan Help Desk.",
+    title: "Business Analyst Position Open",
+    details: `**No. of Vacancies:** Upto 20
+    
+    **Experience:** 0 To 1 year
+    
+    **Qualification:** MBA Finance / Marketing / Operations / Graduate / Any Stream
+    
+    **Job Responsibility:**
+    
+    - Generate clients for subscription of our research services and achieve targets as assigned by the authority.
+    - Understand the requirements of the customer and map them to the available services of the organization or to the expertise and skills within the organization.
+    - Strategize effectively to create new customers on a continuous basis.
+    - Handle customer queries and provide them information regarding the different services of the company.
+    
+    **Skills Required:**
+    - Good communication skills are a must.
+    
+    To apply, please send your resume to hr@tradegyan.co with the subject "Apply For Research Services Subscription".`,
   },
 ];
 const JobPosting = ({ job, isOpen, onClick }) => {
@@ -135,10 +65,25 @@ const Career = () => {
   return (
     <>
       <Navbar />
+      <div>
+        <p className="text-center text-3xl font-bold">Application Process</p>
+        <div className="max-w-7xl mx-auto p-5 flex flex-col gap-2">
+          <p>
+            1. Read Application Read the application carefully before applying
+            for the position
+          </p>
+          <p>
+            2. Send Your Resume Send Your Resume at hr@tradegyan.co with subject
+            (Apply For Business Analyst)
+          </p>
+          <p>
+            3. Come For Interview If your profile will shortlist for interview,
+            we will call you.
+          </p>
+        </div>
+      </div>
       <div className="max-w-7xl mx-auto p-4">
-        <p className="text-center font-bold text-xl my-5">
-          Frequently Asked Questions
-        </p>
+        <p className="text-center font-bold text-xl my-5">Open Positions</p>
         {jobPostings.map((job, index) => (
           <JobPosting
             key={index}
